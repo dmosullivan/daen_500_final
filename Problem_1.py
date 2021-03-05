941273b1568069a23f1d11abc97e8a5caaf0b32d
dmosullivan <78668311+dmosullivan@users.noreply.github.com>
2021-03-05 11:50:14 -0800

    Add files via upload

---
+++ Problem_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+lower = int(input("Enter lower range limit:"))
+upper = int(input("Enter upper range limit:"))
+for i in range(lower, upper+1):
+   if((i%7==0) & (i%5!=0)):
+      print(i)
+
+
+# In[ ]:
+
+
+
+
