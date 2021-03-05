0b9da22939113f921559e181b580488fb412856c
dmosullivan <78668311+dmosullivan@users.noreply.github.com>
2021-03-05 12:55:17 -0800

    Add files via upload

---
+++ Problem_3.R
@@ -0,0 +1,6 @@
+myCourses <- c("advanced_underwater_basket_weaving_1", "general_chemistry", "introduction_to_the_atom", "DAEN500", "how_to_drive_stick_101", "bird_watching_5068", "Modern_Dance_202", "rebooting_hardware_9000")
+length(myCourses)
+myCourses[1:2]
+myCourses[3:4]
+sort(myCourses)
+sort(myCourses, decreasing = TRUE)
