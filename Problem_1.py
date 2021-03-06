a6251691cbcba9d751212e28c543e87ae0c7b9e3
dmosullivan <78668311+dmosullivan@users.noreply.github.com>
2021-03-06 17:36:08 -0500

    Update Problem_1.py

--- Problem_1.py
+++ Problem_1.py
@@ -7,7 +7,7 @@
 lower = int(input("Enter lower range limit:"))
 upper = int(input("Enter upper range limit:"))
 for i in range(lower, upper+1):
-   if((i%7==0) & (i%5!=0)):
+   if((i%7==0) and (i%5!=0)):
       print(i)
 
 
